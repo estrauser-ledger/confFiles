13cbc564d95927eb49ceb72e001dc728d3d8aed4
Strauser <e.strauser@gmail.com>
2020-06-08 10:57:57 +0200

    init script

---
+++ init.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+declare -A modules
+
+for module in modules/*
+do
+  if [[ -f $module  ]]
+  then
+
+    module_name=`echo $module | sed 's/.*\///g'`
+
+    input=0
+    while [[ ! $input =~ ^[yYnN]?$ ]]
+    do
+      read -p "Install $module_name [Y/n]: " input
+    done
+
+    if [ "$input" = "y" ] || [ "$input" = "Y" ] || [ "$input" = "" ]
+    then
+      modules[$module]=$module_name
+    fi
+  fi
+done
+
+for module in "${!modules[@]}"
+do
+  echo "Installing ${modules[$module]}..."
+  bash $module
+done
+
+
